491cb9b417faa1fc3eec761a4b3a464ff8788e1a
Mark Carrington <markc@data-8.co.uk>
2017-11-03 17:01:05 +0000

    Added merge demo

---
+++ CRMUGDevDemo/MergeDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace MergeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
+            var org = crmSvc.OrganizationServiceProxy;
+
+            var contact1 = org.Retrieve("contact", new Guid("{FB2FB326-A9C0-E711-80DB-00155D007101}"), new ColumnSet("transactioncurrencyid"));
+            var contact2 = org.Retrieve("contact", new Guid("{9F59035D-A9C0-E711-80DB-00155D007101}"), new ColumnSet("transactioncurrencyid"));
+            
+            org.Execute(new MergeRequest
+            {
+                Target = contact1.ToEntityReference(),
+                SubordinateId = contact2.Id,
+                UpdateContent = new Entity("contact")
+                {
+                    ["transactioncurrencyid"] = contact1["transactioncurrencyid"]
+                }
+            });
+        }
+    }
+}
