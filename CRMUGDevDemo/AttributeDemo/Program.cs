e3531f92d4b63def995c92518f265462b09a0828
Matt Beard <mattb@data-8.co.uk>
2017-11-06 17:15:43 +0000

    try to update

--- CRMUGDevDemo/AttributeDemo/Program.cs
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
 
 namespace AttributeDemo
@@ -18,20 +19,13 @@ namespace AttributeDemo
             var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
             var org = crmSvc.OrganizationServiceProxy;
 
-            var createAttrReq = new CreateAttributeRequest()
-            {
-                EntityName = "account",
-                Attribute = new StringAttributeMetadata()
-                {
-                    SchemaName = "address1_brandnewfield",
-                    DisplayName = new Label("Address 1: Custom Field 1", 1033),
-                    RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
-                    Description = new Label("String Attribute", 1033),
-                    MaxLength = 100
-                }
-            };
+            var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("address1_line1"));
+            account1["address1_line1"] = "Line 1";
+            org.Update(account1);
 
-            org.Execute(createAttrReq);
+            var account2 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("address1_brandnewfield"));
+            account2["address1_brandnewfield"] = "brand new value";
+            org.Update(account2);
         }
     }
 }
