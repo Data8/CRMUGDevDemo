d689b1c8c5f25bc63a44e60b0b9d1d1f85ffa7b5
Matt Beard <mattb@data-8.co.uk>
2017-11-06 16:30:52 +0000

    Code Commit

---
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace AttributeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
+            var org = crmSvc.OrganizationServiceProxy;
+
+            var createAttrReq = new CreateAttributeRequest()
+            {
+                EntityName = "account",
+                Attribute = new StringAttributeMetadata()
+                {
+                    SchemaName = "address1_brandnewfield",
+                    DisplayName = new Label("Address 1: Custom Field 1", 1033),
+                    RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
+                    Description = new Label("String Attribute", 1033),
+                    MaxLength = 100
+                }
+            };
+
+            org.Execute(createAttrReq);
+        }
+    }
+}
