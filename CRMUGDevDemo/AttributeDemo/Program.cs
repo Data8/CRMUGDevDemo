584ecfa8a48c67427c907137f414dd75f825fff5
Matt Beard <mattb@data-8.co.uk>
2017-11-07 10:45:58 +0000

    Done

--- CRMUGDevDemo/AttributeDemo/Program.cs
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -16,46 +16,34 @@ namespace AttributeDemo
     {
         static void Main(string[] args)
         {
+            const string attrname = "data8_developerheadaches";
+
             var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
             var org = crmSvc.OrganizationServiceProxy;
 
             var deleteAttrReq = new DeleteAttributeRequest()
             {
                 EntityLogicalName = "account",
-                LogicalName = "address1_brandnewfield"
+                LogicalName = attrname
             };
             org.Execute(deleteAttrReq);
 
-
-            var createAttrReq = new CreateAttributeRequest()
+            var createAttrReq = new UpdateAttributeRequest()
             {
                 EntityName = "account",
                 Attribute = new StringAttributeMetadata()
                 {
-                    SchemaName = "new_address1_brandnewfield",
-                    DisplayName = new Label("Address 1: Custom Field 1", 1033),
+                    SchemaName = attrname,
+                    DisplayName = new Label("Custom Field 1", 1033),
                     RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
                     Description = new Label("String Attribute", 1033),
-                    MaxLength = 100
+                    MaxLength = 26
                 }
             };
 
-            org.Execute(createAttrReq);
-
-            var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("address1_line1"));
-            account1["address1_line1"] = "Line 1 (Take 2)";
+            var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet(attrname));
+            account1[attrname] = "abcdefghijklmnopqrstuvwxyz";
             org.Update(account1);
-
-            var account2 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("address1_brandnewfield"));
-            account2["new_address1_brandnewfield"] = "brand new value (take 2)";
-            org.Update(account2);
-
-            //var deleteNewAttrReq = new DeleteAttributeRequest()
-            //{
-            //    EntityLogicalName = "account",
-            //    LogicalName = "new_address1_brandnewfield"
-            //};
-            //org.Execute(deleteNewAttrReq);
         }
     }
 }
