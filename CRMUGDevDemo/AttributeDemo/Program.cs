4137f30c695db4d39561681841074c2786563a3a
Matt Beard <mattb@data-8.co.uk>
2017-11-07 11:34:00 +0000

    sync

--- CRMUGDevDemo/AttributeDemo/Program.cs
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
@@ -45,6 +46,27 @@ namespace AttributeDemo
             var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet(attrname));
             account1[attrname] = "abcdefghijklmnopqrstuvwxyz";
             org.Update(account1);
+
+            var updatAttrReq = new UpdateAttributeRequest()
+            {
+                EntityName = "account",
+                Attribute = new StringAttributeMetadata()
+                {
+                    LogicalName = attrname,
+                    SchemaName = attrname,
+                    DisplayName = new Label("Custom Field 1", 1033),
+                    RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
+                    Description = new Label("String Attribute", 1033),
+                    MaxLength = 13
+                }
+            };
+
+            org.Execute(updatAttrReq);
+
+            var publishAll = new PublishAllXmlRequest();
+            org.Execute(publishAll);
+
+
         }
     }
 }
