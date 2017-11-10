ab614dac469a7a758db932ef682163be84f6324a
Matt Beard <mattb@data-8.co.uk>
2017-11-10 15:21:51 +0000

    Remove creation

--- CRMUGDevDemo/AttributeDemo/Program.cs
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -20,20 +20,6 @@ namespace AttributeDemo
             var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
             var org = crmSvc.OrganizationServiceProxy;
 
-            var createAttrReq = new CreateAttributeRequest()
-            {
-                EntityName = "account",
-                Attribute = new StringAttributeMetadata()
-                {
-                    SchemaName = "data8_developerheadaches",
-                    DisplayName = new Label("Custom Field 1", 1033),
-                    RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
-                    Description = new Label("String Attribute", 1033),
-                    MaxLength = 26
-                }
-            };
-            org.Execute(createAttrReq);
-
             var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("data8_developerheadaches"));
             account1["data8_developerheadaches"] = "abcdefghijklmnopqrstuvwxyz";
             org.Update(account1);
