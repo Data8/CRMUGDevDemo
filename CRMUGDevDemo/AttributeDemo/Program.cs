650a4afcbe1253cbd80bac3075383dc8d4672e73
Matt Beard <mattb@data-8.co.uk>
2017-11-10 15:28:54 +0000

    Change demo

--- CRMUGDevDemo/AttributeDemo/Program.cs
+++ CRMUGDevDemo/AttributeDemo/Program.cs
@@ -21,14 +21,9 @@ namespace AttributeDemo
             var org = crmSvc.OrganizationServiceProxy;
 
             // Load 1 account and store the value thats safely stored in CRM
-            var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("data8_developerheadaches"));
-            var account1Value = account1.GetAttributeValue<string>("data8_developerheadaches");
-            Console.WriteLine($"data8_developerheadaches: {account1Value}");
-
-            // Try to re-use that value anywhere else
-            var account2 = org.Retrieve("account", new Guid("3494C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("data8_developerheadaches"));
-            account2["data8_developerheadaches"] = account1.GetAttributeValue<string>("data8_developerheadaches");
-            org.Update(account2);         
+            var account1 = org.Retrieve("account", new Guid("3894C5BA-A4C0-E711-80DD-00155D00710B"), new ColumnSet("data8_thealphabet", "name"));
+            account1["fax"] = "00000000000";
+            org.Update(account1);
         }
     }
 }
